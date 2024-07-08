83012aa474c713ab8c643fc15a1a66c36925fed1
mohamedmostafa <mohamed@jiva.ai>
2024-07-08 21:00:27 +0100

    version="1.2.5"

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 """
-Author: mmsa12
+@author: mmsa12
 """
 
 import setuptools
 from setuptools.command.install import install as _install
+
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -21,27 +22,26 @@ class Install(_install):
 
 setuptools.setup(
     name="EmoTFIDF",
-    version="1.2.3",
+    version="1.2.5",
     author="mmsa12",
     author_email="mmsa12@gmail.com",
-    description="A library to extract emotions using two methods: 1) Using lexicon-based counting frequency of emotion, "
-                "2) Integrating TFIDF to add context.",
+    description="A library to extract emotions using two methods, 1- Using lexicon based, counting frequency of emotion"
+                "2- Integrating TFIDF to add a context"
+                "Note that lexicon license is for research purposes only.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/mmsa/emotfidf",
     packages=setuptools.find_packages(),
     install_requires=['nltk'],
     include_package_data=True,
+    py_modules=["emotfidf"],
     setup_requires=['nltk'],
-    tests_require=['pytest'],
-    extras_require={
-        'dev': ['pytest'],
-    },
-    package_data={'emotfidf': ['emotions_lex.json']},
+    package_data={'project': ['emotions_lex.json']},
     classifiers=[
         "Programming Language :: Python :: 3.6",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=2.7',
+    sklearn='>=1.0.x'
 )

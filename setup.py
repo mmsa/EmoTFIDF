28bae6478d661458cdf3cef3c4ac6ee2efd82688
mohamedmostafa <mohamed@jiva.ai>
2024-07-08 20:29:06 +0100

    python 3.10

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ class Install(_install):
 
 setuptools.setup(
     name="EmoTFIDF",
-    version="1.2.2",
+    version="1.2.3",
     author="mmsa12",
     author_email="mmsa12@gmail.com",
     description="A library to extract emotions using two methods: 1) Using lexicon-based counting frequency of emotion, "
